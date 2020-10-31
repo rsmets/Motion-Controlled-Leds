f6273f0dc76821a7db87cff46c8388bca5e34ee7
Ray Smets <rayjsmets@gmail.com>
2020-10-30 23:50:12 -0700

    [Fire Animations] static fire animations added while still reacting to spinning.

--- Prop_Mover/Prop_Mover.ino
+++ Prop_Mover/Prop_Mover.ino
@@ -474,14 +474,26 @@ void doLeds()
                 // Serial.print(" ");
         }
 
+        // Dynamic user input animations
+        // if (animationNumber % ANIMATION_COUNT ==  0) {
+        //         dripRead();
+        // }
+        // else if (animationNumber % ANIMATION_COUNT ==  1) {
+        //         rainbowPaintRead();
+        // }
+        // else {
+        //         sparklerRead();
+        // }
+        
+        // Static fire animations
         if (animationNumber % ANIMATION_COUNT ==  0) {
-                dripRead();
+                sparklerFireRead(10);
         }
         else if (animationNumber % ANIMATION_COUNT ==  1) {
-                rainbowPaintRead();
+                sparklerFireRead(125);
         }
         else {
-                sparklerRead();
+                sparklerFireRead(250);
         }
 
         if(DEBUG_OUTPUT && YAW_ANIMATION_CONTROL) {
@@ -844,6 +856,93 @@ void updateSpeed(int multiplier)
                 speed = yaw * multiplier;
         }
 }
+
+// COOLING: How much does the air cool as it rises?
+// Less cooling = taller flames.  More cooling = shorter flames.
+// Default 50, suggested range 20-100 
+#define COOLING  55
+
+// SPARKING: What chance (out of 255) is there that a new spark will be lit?
+// Higher chance = more roaring fire.  Lower chance = more flickery fire.
+// Default 120, suggested range 50-200.
+#define SPARKING 120
+bool gReverseDirection = false;
+
+void sparklerFireReadOg()
+{
+// Array of temperature readings at each simulation cell
+  static byte heat[NUM_LEDS];
+
+  // Step 1.  Cool down every cell a little
+    for( int i = 0; i < NUM_LEDS; i++) {
+      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    }
+  
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for( int k= NUM_LEDS - 1; k >= 2; k--) {
+      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+    }
+    
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    if( random8() < SPARKING ) {
+      int y = random8(7);
+      heat[y] = qadd8( heat[y], random8(160,255) );
+    }
+
+    // Step 4.  Map from heat cells to LED colors
+    for( int j = 0; j < NUM_LEDS; j++) {
+      CRGB color = HeatColor( heat[j]);
+      int pixelnumber;
+      if( gReverseDirection ) {
+        pixelnumber = (NUM_LEDS-1) - j;
+      } else {
+        pixelnumber = j;
+      }
+      leds[pixelnumber] = color;
+    }
+
+    FastLED.show(); // display this frame
+}
+
+void sparklerFireRead(uint8_t fireBrightness)
+{
+// Array of temperature readings at each simulation cell
+  static byte heat[NUM_LEDS];
+
+  // Step 1.  Cool down every cell a little
+    for( int i = 0; i < NUM_LEDS; i++) {
+      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    }
+  
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for( int k= NUM_LEDS - 1; k >= 2; k--) {
+      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+    }
+    
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    if( random8() < SPARKING ) {
+      int y = random8(7);
+      heat[y] = qadd8( heat[y], random8(100,255) );
+    }
+
+     // Step 4.  Map from heat cells to LED colors
+    for( int j = 0; j < NUM_LEDS; j++) {
+      // Scale the heat value from 0-255 down to 0-240
+      // for best results with color palettes.
+      byte colorindex = scale8( heat[j], 240);
+      CRGB color = ColorFromPalette( CRGBPalette16( CRGB::Black, CRGB::Red, CRGB::Orange,  CRGB::White), colorindex, fireBrightness);
+      int pixelnumber;
+      if( gReverseDirection ) {
+        pixelnumber = (NUM_LEDS-1) - j;
+      } else {
+        pixelnumber = j;
+      }
+      leds[pixelnumber] = color;
+    }
+
+    FastLED.show(); // display this frame
+}
+
 //SPARKLER: a brush seeding sparkles
 // Animation
 void sparklerRead()
